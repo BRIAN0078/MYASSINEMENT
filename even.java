8a967310e25364a0f1a3dfafc0dafebdc4eb2f15
BRIAN0078 <124643833+BRIAN0078@users.noreply.github.com>
2023-02-07 17:54:08 -0500

    Add files via upload

---
+++ even.java
@@ -0,0 +1,9 @@
+public class even {
+    public static void main(String[] args) {
+
+        System.out.println("odd number 1-20");
+        for (int i=2; i<=20; i+=3){
+            System.out.println("odd number from 1 to 20:"+i);
+        }
+    }
+}
