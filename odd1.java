f4096c0f0d2a23fcc10faa0516250ca8a4cd0a06
BRIAN0078 <124643833+BRIAN0078@users.noreply.github.com>
2023-02-07 18:05:11 -0500

    Add files via upload

---
+++ odd1.java
@@ -0,0 +1,9 @@
+public class odd1 {
+    public static void main(String[] args) {
+
+        System.out.println("odd number 2-30");
+        for (int i=3; i<=30; i+=2){
+            System.out.println("odd number from 2 to 20:"+i);
+        }
+    }
+}
