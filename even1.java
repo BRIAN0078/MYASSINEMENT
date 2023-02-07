29e24346de3347461e18a2a6c9bdbe3c61629fb4
BRIAN0078 <124643833+BRIAN0078@users.noreply.github.com>
2023-02-07 18:04:56 -0500

    Add files via upload

---
+++ even1.java
@@ -0,0 +1,9 @@
+public class even1 {
+    public static void main(String[] args) {
+
+        System.out.println("even number 1-20");
+        for (int i=2; i<=20; i+=2){
+            System.out.println("even number from 1 to 20:"+i);
+        }
+    }
+}
